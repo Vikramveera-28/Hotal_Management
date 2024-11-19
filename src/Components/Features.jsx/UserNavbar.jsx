616d0ed72b6ef6ad8665729146d6f1f8b2dfbf6a
Vikram Kumar <121078328+Vikramveera-28@users.noreply.github.com>
2024-11-19 17:56:09 +0530

    Update UserNavbar.jsx

--- src/Components/Features.jsx/UserNavbar.jsx
+++ src/Components/Features.jsx/UserNavbar.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export const UserNavbar = () => {
     return (
         <main>
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark" aria-label="Fourth navbar example">
-                <div className="container-fluid">
-                <Link className="navbar-brand">V-Hotal</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarsExample04">
-                    <ul className="navbar-nav mx-auto mb-2 mb-md-0">
-                        <li className="nav-item"><Link to={'/user/home'} className="nav-link active">Home</Link></li>
-                        <li className="nav-item"><Link to={'/user/restaurant'} className="nav-link active">Restaurant</Link></li>
-                        <li className="nav-item"><Link to={'/user/laundary'} className="nav-link active">Laundary</Link></li>
-                        <li className="nav-item"><Link to={'/user/game'} className="nav-link active">Game</Link></li>
-                        <li className="nav-item"><Link to={'/user/bill'} className="nav-link active">Bill</Link></li>
-                    </ul>
-                    <ul className="navbar-nav ms-auto mb-2 mb-md-0">
-                        <li className="nav-item"><Link to={'/'} className="nav-link">LogOut</Link></li>
-                    </ul>
-                </div>
-                </div>
-            </nav>
+            <Navbar expand="md" bg="dark" className="position-fixed top-0 w-100" data-bs-theme="dark">
+            <Container>
+                <Navbar.Brand href="#home">V-Hotal</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <li href="#home"><Link className="nav-link active" to={'/user/home'}>Home</Link></li>
+                    <li href="#home"><Link className="nav-link active" to={'/user/restaurant'}>Restaurant</Link></li>
+                    <li href="#home"><Link className="nav-link active" to={'/user/laundary'}>Laundary</Link></li>
+                    <li href="#home"><Link className="nav-link active" to={'/user/game'}>Games</Link></li>
+                    <li href="#home"><Link className="nav-link active" to={'/user/bill'}>Bill</Link></li>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
             <Outlet />
         </main>
     )
