354fffec0a27e6b0881fd90815e84c39c38feb08
Vikramveera-28 <vikramkumaru28@gmail.com>
2025-02-19 21:04:27 +0530

    Change api path

--- src/Api/apiUrl.js
+++ src/Api/apiUrl.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    // baseURL: 'https://json-server-vx6k.onrender.com'
-    baseURL: 'http://localhost:3500'
+    baseURL: 'https://json-server-vx6k.onrender.com'
+    // baseURL: 'http://localhost:3500'
 })
